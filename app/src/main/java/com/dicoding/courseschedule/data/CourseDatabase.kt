4d27d3240ac8bc9cd4ebb72ac3fed20347e84248
kevinngl <if320021@students.del.ac.id>
2023-07-13 16:00:33 +0700

    push

--- app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
+++ app/src/main/java/com/dicoding/courseschedule/data/CourseDatabase.kt
@@ -1,10 +1,14 @@
 package com.dicoding.courseschedule.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 //TODO 3 : Define room database class
+@Database(entities = [Course::class],
+    version = 1,
+    exportSchema = false)
 abstract class CourseDatabase : RoomDatabase() {
 
     abstract fun courseDao(): CourseDao
@@ -17,7 +21,7 @@ abstract class CourseDatabase : RoomDatabase() {
         fun getInstance(context: Context): CourseDatabase {
             return synchronized(this){
                 instance ?: Room.databaseBuilder(context, CourseDatabase::class.java, "courses.db")
-                        .build()
+                    .build()
             }
         }
 
